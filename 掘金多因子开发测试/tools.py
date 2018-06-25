384502c0003eb26c99727bd28ef446fdac29862e
08zhang <395871987@qq.com>
2018-06-25 09:10:27 +0800

    update

--- 掘金多因子开发测试/tools.py
+++ 掘金多因子开发测试/tools.py
@@ -1,24 +1,44 @@
 # 自己使用的工具函数类
 import QuantLib as ql
+import pandas as pd
+from WindPy import w
 
 
-def get_trading_date_from_now(date_now, diff_days):
+def get_trading_date_from_now(date_now, diff_periods, period=ql.Days):
     calculation_date = ql.Date(int(date_now.split('-')[2]), int(date_now.split('-')[1]), int(date_now.split('-')[0]))
     calendar = ql.China()
-    date_diff = calendar.advance(calculation_date, diff_days, ql.Days).to_date().strftime('%Y-%m-%d')
+    date_diff = calendar.advance(calculation_date, diff_periods, period).to_date().strftime('%Y-%m-%d')
     return date_diff
 
 
 def list_gm2wind(list_gm):
     gm2wind_dict = {'SHSE': 'SH', 'SZSE': 'SZ', 'CFFEX': 'CFE', 'SHFE': 'SHF', 'DCE': 'DCE', 'CZCE': 'CZC', 'INE': 'INE'}
-    list_wind = [temp.split('.')[1]+gm2wind_dict[temp.split('.')[0]] for temp in list_gm]
+    list_wind = [temp.split('.')[1]+'.'+gm2wind_dict[temp.split('.')[0]] for temp in list_gm]
     return list_wind
 
 
 def list_wind2gm(list_wind):
     wind2gm_dict = {'SH': 'SHSE', 'SZ': 'SZSE', 'CFE': 'CFFEX', 'SHF': 'SHFE', 'DCE': 'DCE', 'CZC': 'CZCE', 'INE': 'INE'}
-    list_gm = [wind2gm_dict[temp.split('.')[1]]+temp.split('.')[0] for temp in list_wind]
+    list_gm = [wind2gm_dict[temp.split('.')[1]]+'.'+temp.split('.')[0] for temp in list_wind]
     return list_gm
+
+
+def get_factor_from_wind(code_list, factor_list, factor_coeff_list, date):
+    # 还需加入本地存储机制，本地有的数据可以加快读取
+    w.start()
+    code_list = list_gm2wind(code_list)  # code_list为gm中的格式
+    factors = ','.join(factor_list)
+    factors_coeffs = ','.join(factor_coeff_list)
+    df_list = []
+    for code in code_list:
+        data_from_wind = w.wsd(code, factors, date, date, factors_coeffs)
+        columns = data_from_wind.Fields
+        index = [code]
+        data = [[temp[0] for temp in data_from_wind.Data]]
+        code_df = pd.DataFrame(data=data, index=index, columns=columns)
+        df_list.append(code_df)
+    df = pd.concat(df_list)
+    return df
 
 
 if __name__ == '__main__':
--- 掘金多因子开发测试/多因子开发测试1.py
+++ 掘金多因子开发测试/多因子开发测试1.py
@@ -1,11 +1,13 @@
 from gm.api import *
-from tools import get_trading_date_from_now
+import QuantLib as ql
+from tools import get_trading_date_from_now, get_factor_from_wind
 
 # 回测的基本参数的设定
 BACKTEST_START_DATE = '2017-02-27'  # 回测开始日期
 BACKTEST_END_DATE = '2018-06-20'  # 回测结束日期，测试结束日期不运用算法
 INDEX = 'SHSE.000016'  # 股票池代码
-FACTOR_LIST = []  # 需要获取的因子列表
+FACTOR_LIST = ['fa_ocftoor_ttm', 'fa_orgr_ttm', 'fa_roaebit_ttm', 'tech_ar']  # 需要获取的因子列表
+FACROT_COEFF_LIST = []  # 需要获取的因子列表的相关参数
 TRADING_DATE = '10'  # 每月的调仓日期，非交易日去下一个最近的交易日
 
 
@@ -26,14 +28,19 @@ def init(context):
 
 def algo(context):
     date_now = context.now.strftime('%Y-%m-%d')
-    date_previous = get_trading_date_from_now(date_now, -1)  # 前一个交易日，用于获取因子数据的日期
-    date_trading = get_trading_date_from_now(date_now.split('-')[0]+'-'+date_now.split('-')[1]+'-'+TRADING_DATE, 0)  # 调仓日期，判断是否调仓
+    date_previous = get_trading_date_from_now(date_now, -1, ql.Days)  # 前一个交易日，用于获取因子数据的日期
+    date_trading = get_trading_date_from_now(date_now.split('-')[0]+'-'+date_now.split('-')[1]+'-'+TRADING_DATE, 0, ql.Days)  # 调仓日期，判断是否调仓
     if date_now != date_trading:  # 非调仓日
         pass
     else:  # 调仓日执行算法
         constituents = get_history_constituents(INDEX, start_date=date_now, end_date=date_now)[0]['constituents'].keys()
-        constituents = list(constituents)
-        print(date_now, date_trading)
+        constituents = list(constituents)  # 获取当日的股票池组成
+        # 取过去几个月计算的周期
+        date_1month_ago = get_trading_date_from_now(get_trading_date_from_now(date_now, -1, ql.Months), -1, ql.Days)
+        date_2month_ago = get_trading_date_from_now(get_trading_date_from_now(date_now, -2, ql.Months), -1, ql.Days)
+        date_3month_ago = get_trading_date_from_now(get_trading_date_from_now(date_now, -3, ql.Months), -1, ql.Days)
+        print(date_now, date_previous, date_1month_ago, date_2month_ago, date_3month_ago)
+        # factor_df = get_factor_from_wind(constituents, FACTOR_LIST, FACROT_COEFF_LIST, date_previous)
 
 
 def on_backtest_finished(context, indicator):
