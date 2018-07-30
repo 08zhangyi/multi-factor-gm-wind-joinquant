9e0feb23ce43fe0685619ac5e4bf4ce9c4081ff0
08zhang <395871987@qq.com>
2018-07-30 15:52:45 +0800

    update

---
+++ 市场分析工具/基金成分分析/手动导数据分析/cvx_opt示例.py
@@ -0,0 +1,20 @@
+from cvxopt import matrix, solvers
+import numpy as np
+
+'''
+问题描述：
+minimize xQx+px
+subject Gx <= h
+        Ax = b
+注：Q=[[2, .5], [.5, 1]]，即xQx=2x1^2+x^2+x1*x2
+'''
+
+Q = 2*matrix(np.array([[2, .5], [.5, 1]])) # 一定要乘以2
+p = matrix([1.0, 1.0])
+G = matrix([[-1.0, 0.0], [0.0, -1.0]])
+h = matrix([0.0, 0.0])
+A = matrix([1.0, 1.0], (1, 2))
+b = matrix(1.0)
+
+sol=solvers.qp(Q, p, G, h, A, b)
+print(sol['x'], sol['y'])
---
+++ 市场分析工具/基金成分分析/手动导数据分析/手动求解.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from cvxopt import matrix, solvers
+
+df = pd.read_csv('data\\data2.csv', index_col=0)
+return_values = df.values
+fundation_values = return_values[:, 0:1]
+index_values = return_values[:, 1:]
+print(fundation_values.shape, index_values.shape)
+x_szie = index_values.shape[1]
+
+Q = np.zeros((29, 29))
+p = np.zeros(29)
+G = np.zeros((29, 29))
+h = np.zeros(29)
+A = np.ones((1, 29))
+b = np.ones(1)
+
+for i in range(x_szie):
+    for j in range(x_szie):
+        if i == j:
+            Q[i, j] = np.sum(index_values[:, i] * index_values[:, j])
+            G[i, j] = -1
+        else:
+            Q[i, j] = np.sum(index_values[:, i] * index_values[:, j])
+for i in range(x_szie):
+    p[i] = -2 * np.sum(fundation_values[:, 0] * index_values[:, i])
+
+Q = matrix(Q)
+p = matrix(p)
+G = matrix(G)
+h = matrix(h)
+A = matrix(A)
+b = matrix(b)
+
+sol=solvers.qp(Q, p, G, h, A, b)
+x_sol = np.array(sol['x']).transpose()[0]
+for x in x_sol:
+    print(x)
