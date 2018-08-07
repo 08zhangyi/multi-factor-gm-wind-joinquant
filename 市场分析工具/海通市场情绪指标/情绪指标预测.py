2e03040143ea5c7d0132944598b22fb6b321bdf2
08zhang <395871987@qq.com>
2018-08-07 15:08:46 +0800

    update

--- 市场分析工具/海通市场情绪指标/情绪指标预测.py
+++ 市场分析工具/海通市场情绪指标/情绪指标预测.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.api import qqplot
 
 # 统计指标对应的信息位置
 loc_dict = {'A股日换手率（年化五个交易日平滑，左轴）': 'Unnamed: 8',
@@ -7,9 +12,47 @@ loc_dict = {'A股日换手率（年化五个交易日平滑，左轴）': 'Unnam
             'PE倒数减十年期国债收益率（%，左轴）': 'Unnamed: 26',
             '恒生AH股溢价指数(%，左轴）': 'Unnamed: 30',
             '信用利差（%，左轴）': 'Unnamed: 44'}
-N_history = 200  # 预测数据需要的历史数据长度
+N_history = 550  # 预测数据需要的历史数据长度
 df = pd.read_excel('data\\海通策略指标.xlsx')
 
-for key in loc_dict.keys():
-    time_series = df[loc_dict[key]].values[N_history:0:-1]
-    print(time_series)
+key = 'A股日换手率（年化五个交易日平滑，左轴）'
+time_series_ori = df[loc_dict[key]].values[N_history:0:-1]
+time_series = np.diff(time_series_ori, 1)
+time_series = list(time_series)
+print(len(time_series))
+
+# fig = plt.figure(figsize=(12, 8))
+# ax1 = fig.add_subplot(211)
+# fig = sm.graphics.tsa.plot_acf(time_series, lags=40, ax=ax1)
+# ax2 = fig.add_subplot(212)
+# fig = sm.graphics.tsa.plot_pacf(time_series, lags=40, ax=ax2)
+# plt.show()
+# print(adfuller(time_series))
+
+arma_mod = sm.tsa.ARMA(time_series, (5, 2)).fit()
+# print(arma_mod.aic, arma_mod.bic, arma_mod.hqic)
+
+resid = list(arma_mod.resid)
+fig = plt.figure(figsize=(12, 8))
+ax1 = fig.add_subplot(211)
+fig = sm.graphics.tsa.plot_acf(resid, lags=40, ax=ax1)
+ax2 = fig.add_subplot(212)
+fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
+plt.show()
+
+print(sm.stats.durbin_watson(arma_mod.resid))
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
+fig = qqplot(np.array(resid), line='q', ax=ax, fit=True)
+plt.show()
+
+predict_sunspots = arma_mod.predict(N_history-1, N_history+3, dynamic=True)
+print(predict_sunspots)
+predict_sunspots = predict_sunspots.cumsum()
+predict_sunspots = time_series_ori[-1] + predict_sunspots
+print(predict_sunspots)
+time_series_ori_predict = np.r_[time_series_ori, predict_sunspots]
+plt.plot(range(len(time_series_ori)), time_series_ori, 'b')
+plt.plot(range(len(time_series_ori), len(time_series_ori)+5), predict_sunspots, 'r')
+plt.show()
