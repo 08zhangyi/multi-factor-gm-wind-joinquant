ce66f64ec62abc504de08f2c0ecadf1f4fe8ae8b
08zhang <395871987@qq.com>
2018-08-09 13:49:03 +0800

    update

--- 市场分析工具/海通市场情绪指标/情绪指标预测-精简信息提取.py
+++ 市场分析工具/海通市场情绪指标/情绪指标预测-精简信息提取.py
@@ -20,6 +20,9 @@ statistics_dict = {'A股日换手率（年化五个交易日平滑，左轴）':
 N_history = 500  # 预测数据需要的历史数据长度
 df = pd.read_excel('data\\海通策略指标.xlsx')
 
+plot_number = len(statistics_dict)
+i = 0
+plt.figure(figsize=(8, 32))
 for key in statistics_dict.keys():
     time_series_ori = df[loc_dict[key]].values[N_history:0:-1]
     time_series = list(np.diff(time_series_ori, statistics_dict[key][0]))
@@ -31,7 +34,10 @@ for key in statistics_dict.keys():
         predict = predict.cumsum()
         predict = time_series_ori[-1] + predict
     time_series_ori_predict = np.r_[time_series_ori, predict]
+    plt.subplot(plot_number, 1, i+1)
+    i += 1
     plt.plot(range(len(time_series_ori)), time_series_ori, 'b')
     plt.plot(range(len(time_series_ori), len(time_series_ori) + 5), predict, 'r')
     plt.title(key)
-    plt.show()  # 输出预测图
+
+plt.show()  # 输出预测图
