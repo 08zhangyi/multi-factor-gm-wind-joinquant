52dce56558903bb3b6e81bf54e663822f5164c10
08zhang <395871987@qq.com>
2018-08-15 14:27:37 +0800

    update

--- 单因子研究/单因子研究工具/single_factor_backtest_group.py
+++ 单因子研究/单因子研究工具/single_factor_backtest_group.py
@@ -3,9 +3,9 @@ import QuantLib as ql
 import pandas as pd
 from WindPy import w
 import json
+import numpy as np
 # 引入工具函数和学习器
-from tools import get_trading_date_from_now, get_factor_from_wind, get_return_from_wind, delete_data_cache, sort_data, list_wind2jq
-from learning_model import OrdinaryLinearRegression
+from tools import get_trading_date_from_now, list_gm2wind, list_wind2jq
 # 引入因子类路径
 import sys
 sys.path.append('D:\\programs\\多因子策略开发\\单因子研究')
@@ -13,13 +13,13 @@ from single_factor import PE
 
 # 俺们但一字分组测试盈亏效果的代码
 # 回测的基本参数的设定
-BACKTEST_START_DATE = '2017-02-27'  # 回测开始日期
-BACKTEST_END_DATE = '2018-07-23'  # 回测结束日期，测试结束日期不运用算法
-INDEX = 'SHSE.000016'  # 股票池代码
-FACTOR_LIST = [PE]  # 需要获取的因子列表，用单因子研究中得模块
+BACKTEST_START_DATE = '2013-08-01'  # 回测开始日期
+BACKTEST_END_DATE = '2018-08-14'  # 回测结束日期，测试结束日期不运用算法
+INDEX = 'SHSE.000300'  # 股票池代码
+FACTOR = PE  # 需要获取的因子列表，用单因子研究中得模块
+FACTOR_COEFF = {}  # 因子获取的具体参数
+QUANTILE = [0.8, 1.0]  # 因子分组的分位数
 TRADING_DATE = '10'  # 每月的调仓日期，非交易日寻找下一个最近的交易日
-HISTORY_LENGTH = 3  # 取得的历史样本的周期数
-STOCK_NUMBER = 10  # 选股数量
 
 # 用于记录调仓信息的字典
 stock_dict = {}
@@ -36,7 +36,7 @@ while True:
     i += 1
     if date_now == BACKTEST_END_DATE:
         break
-BACKTEST_START_DATE = trading_date_list[HISTORY_LENGTH]  # 调整回测起始日为第一次调仓的日子
+BACKTEST_START_DATE = trading_date_list[0]  # 调整回测起始日为第一次调仓的日子
 
 
 def init(context):
@@ -61,37 +61,19 @@ def algo(context):
         pass  # 预留非调仓日的微调空间
     else:  # 调仓日执行算法
         print(date_now+'日回测程序执行中...')
-        code_list = list(get_history_constituents(INDEX, start_date=date_previous, end_date=date_previous)[0]['constituents'].keys())
-        I = trading_date_list.index(date_now)
-        trading_dates = trading_date_list[I-HISTORY_LENGTH:I+1]
-        data_dfs = []
-        for i in range(len(trading_dates)-1):
-            date_start = get_trading_date_from_now(trading_dates[i], -1, ql.Days)  # 计算因子值的日子，买入前一日的因子值
-            date_end = get_trading_date_from_now(trading_dates[i+1], -1, ql.Days)  # 计算收益率到期的日子-收盘
-
-            factors_df = get_factor_from_wind(code_list, FACTOR_LIST, date_start)  # 获取因子
-            return_df = get_return_from_wind(code_list, date_start, date_end)
-            factors_df_and_return_df = pd.concat([factors_df, return_df], axis=1).dropna()  # 去掉因子或者回报有空缺值的样本
-            factors_df_and_return_df = sort_data(factors_df_and_return_df)  # 使用排序数据作为输入
-            data_dfs.append(factors_df_and_return_df)
-        factors_return_df = pd.concat(data_dfs, axis=0)  # 获取的最终训练数据拼接，return为目标
-        # 根据data_df训练模型
-        model = OrdinaryLinearRegression()
-        model.fit(factors_return_df)
-        # 根据factor_date_previous选取股票
-        factor_date_previous_df = get_factor_from_wind(code_list, FACTOR_LIST, date_previous).dropna()
-        sorted_codes = model.predict(factor_date_previous_df)  # 获取预测收益率从小到大排序的股票列表
-        sorted_codes = list_wind2jq(sorted_codes)
-        # 根据股票列表下单
-        stock_codes = sorted_codes[-STOCK_NUMBER:]
+        code_list = list_gm2wind(list(get_history_constituents(INDEX, start_date=date_previous, end_date=date_previous)[0]['constituents'].keys()))
+        factor = FACTOR(date_previous, code_list, **FACTOR_COEFF)  # 读取单因子的代码
+        df = factor.get_factor().dropna()
+        quantiles = df.quantile(QUANTILE).values  # 取对应的分位数值
+        stock_codes = list(df[(df[factor.factor_name] >= quantiles[0][0]) & (df[factor.factor_name] < quantiles[1][0])].index.values)
+        stock_codes = list_wind2jq(stock_codes)
         stock_now = {}
-        for stock_code in stock_codes:  # 平均持仓持股
-            stock_now[stock_code] = 1./STOCK_NUMBER
+        for stock_code in stock_codes:
+            stock_now[stock_code] = 1.0 / len(stock_codes)
         stock_dict[date_now] = stock_now
 
 
 def on_backtest_finished(context, indicator):
-    delete_data_cache()  # 删除缓存中的数据，可手动选取是否删除
     # 输出回测指标
     print(indicator)
     stock_json = json.dumps(stock_dict)
@@ -102,7 +84,7 @@ def on_backtest_finished(context, indicator):
 
 if __name__ == '__main__':
     run(strategy_id='4d2f6b1c-8f0a-11e8-af59-305a3a77b8c5',
-        filename='多因子开发测试1.py',
+        filename='single_factor_backtest_group.py',
         mode=MODE_BACKTEST,
         token='d7b08e7e21dd0315a510926e5a53ade8c01f9aaa',
         backtest_initial_cash=10000000,
