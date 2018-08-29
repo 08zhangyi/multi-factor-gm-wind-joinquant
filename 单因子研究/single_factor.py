157191c794328bc3bcf9cef25212282283576dd5
08zhang <395871987@qq.com>
2018-08-29 10:40:38 +0800

    update

--- 单因子研究/single_factor.py
+++ 单因子研究/single_factor.py
@@ -862,12 +862,25 @@ class StockPledgeRatio(SingleFactorReasearch):
         return list
 
 
+# 申万一级行业
+class SW1Industry(SingleFactorReasearch):
+    def __init__(self, date, code_list):
+        factor_name = '申万一级行业'
+        super().__init__(date, code_list, factor_name)
+
+    def _calculate_factor(self):
+        date_list = self.date
+        fa_uncurdebttoworkcap = np.array(w.wss(self.code_list, "indexcode_sw", "tradeDate="+''.join(date_list)+";industryType=1").Data[0])
+        fa_uncurdebttoworkcap = pd.DataFrame(data=fa_uncurdebttoworkcap, index=self.code_list, columns=[self.factor_name])
+        return fa_uncurdebttoworkcap
+
+
 if __name__ == '__main__':
     date = '2017-05-09'
     w.start()
     # code_list = w.wset("sectorconstituent", "date=" + date + ";windcode=000300.SH").Data[1]  # 沪深300动态股票池
     code_list = ['000001.SZ', '000002.SZ']
-    factor_model = PB(date, code_list)
+    factor_model = SW1Industry(date, code_list)
     df = factor_model.get_factor()
     # df.to_csv('temp1.csv')
     print(df)
