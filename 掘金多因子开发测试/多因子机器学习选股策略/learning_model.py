8cc48f563143879be777651eece0c8569bf6aa18
08zhang <395871987@qq.com>
2018-12-10 10:54:26 +0800

    机器学习学习器更新

--- 掘金多因子开发测试/多因子机器学习选股策略/learning_model.py
+++ 掘金多因子开发测试/多因子机器学习选股策略/learning_model.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn import linear_model
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
 
 # 针对sklearn的学你模型包装的学习器基类
@@ -16,6 +18,7 @@ class BaseLearner(object):
         return None
 
 
+# SKLearn回归器基类
 class BaseLearnerForSKLearnRegressor(BaseLearner):
     def __init__(self, select_number=None, select_ratio=None):
         super().__init__()
@@ -48,7 +51,45 @@ class OrdinaryLinearRegression(BaseLearnerForSKLearnRegressor):
         self.regr = linear_model.LinearRegression()
 
 
-class AdaBoost_DecisionTree_Regresor(BaseLearnerForSKLearnRegressor):
+class AdaBoostDecisionTreeRegresor(BaseLearnerForSKLearnRegressor):
     def __init__(self, max_depth=4, n_estimators=20, random_state=np.random.RandomState(1024), select_number=None, select_ratio=None):
         super().__init__(select_number, select_ratio)
         self.regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth=max_depth), n_estimators=n_estimators, random_state=random_state)
+
+
+# SKLearn分类器基类
+class BaseLearnerForSKLearnClassifier(BaseLearner):
+    def __init__(self, select_number=None, select_ratio=None):
+        super().__init__()
+        self.clf = None
+        self.select_number = select_number
+        self.select_ratio = select_ratio
+
+    def fit(self, factors_return_df):
+        # factors_return_df不需要预处理，代码中将大于均值的设为1，小于均值的设为0
+        return_data_train = factors_return_df['return'] > factors_return_df['return'].mean()
+        return_data_train = return_data_train.replace({True: 1, False: 0})  # 用1和0替换收益率数据
+        return_data_train = return_data_train.values
+        del factors_return_df['return']
+        factors_data_train = factors_return_df.values
+        self.clf.fit(factors_data_train, return_data_train)  # 常规训练
+
+    def predict(self, factors_df):
+        factors_data_test = factors_df.values
+        return_data_test = self.clf.predict_proba(factors_data_test)[:, 1]  # 预测标签为1的概率
+        sorted_codes = list(factors_df.index[np.argsort(return_data_test)])  # 预测概率由大到小排序
+        if self.select_number is not None:
+            select_codes = sorted_codes[-self.select_number:]
+        elif self.select_ratio is not None:
+            select_codes = sorted_codes[-int(len(sorted_codes)*self.select_ratio):]
+        else:
+            raise Exception('选股数量或者选股比例须给出')
+        return select_codes
+
+
+class SVMClassifier(BaseLearnerForSKLearnClassifier):
+    def __init__(self, cv=5, select_number=None, select_ratio=None):
+        super().__init__(select_number, select_ratio)
+        self.params = {'C': range(1, 10, 2), 'gamma': [x / 100. for x in range(1, 10, 2)],
+                  'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}  # 参数优化
+        self.clf = GridSearchCV(SVC(probability=True), self.params, cv=cv)
