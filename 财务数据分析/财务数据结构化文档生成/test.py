b23bc768d38123ba7e6bddcf4a07e3a2a8ce6cb8
08zhang <395871987@qq.com>
2019-03-28 15:31:22 +0800

    主观分析模板，总结性分析

---
+++ 财务数据分析/财务数据结构化文档生成/test.py
@@ -0,0 +1,5 @@
+import prettytable
+
+x = prettytable.PrettyTable(["名字", "数学成绩", "语文成绩", "计算机成绩"])
+x = str(x)
+print(x)
--- 财务数据分析/财务数据结构化文档生成/个股分析模板.py
+++ 财务数据分析/财务数据结构化文档生成/个股分析模板.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pygal
 from WindPy import w
 import tushare as ts
+import prettytable
 import sys
 sys.path.append('D:\\programs\\多因子策略开发\\单因子研究')
 TS_TOKEN = '9668f6b57f4e3fe1199446a9c7b251d553963832bbf6e411b8065ea2'
@@ -65,20 +66,23 @@ class 个股主营业务分析_按产品(个股分析模板):
         df['bz_sales_ratio'] = df['bz_sales'] / np.sum(df['bz_sales'].values)  # 计算营业收入占比
         df['bz_profit_ratio'] = df['bz_profit'] / df['bz_sales']  # 计算毛利率
         # 文字生成部分
+        table = prettytable.PrettyTable(['业务名称', '销售收入', '销售成本', '销售毛利', '销售毛利率', '销售收入占比'])
         for i in range(len(df)):
             data = df.iloc[i].values
-            text_temp = '业务名称：%s，\t销售收入：%.2f万元，\t销售成本：%.2f万元，\t销售毛利：%.2f万元，\t销售毛利率：%.2f%%，\t销售收入占比：%.2f%%；\n'
-            text_temp = text_temp % (data[2], data[3]/10000.0, data[5]/10000.0, data[4]/10000.0, data[8]*100.0, data[7]*100.0)
-            text = text + text_temp
+            table.add_row(['%s' % data[2], '%.2f万元' % (data[3]/10000.0), '%.2f万元' % (data[5]/10000.0), '%.2f万元' % (data[4]/10000.0), '%.2f%%' % (data[8]*100.0), '%.2f%%' % (data[7]*100.0)])
+        text = text + str(table) + '\n'
         # 画图部分
         pie_chart = pygal.Pie()
         pie_chart.title = self.code_name + '（' + self.code + '）的主营业务百分比构成图（数据截止为'+date+'）'
         for i in range(len(df)):
             data = df.iloc[i].values
             pie_chart.add(data[2], data[7]*100.0)
-        # 总结内容，待开发
-        summary_text = self._get_summary(df, self.code_name)
-        text = text + summary_text
+        # 总结内容
+        if len(df) > 0:
+            summary_text = self._get_summary(df, self.code_name)
+            text = text + summary_text
+        else:  # 无数据披露的情形
+            text = text + '无数据！\n'
         # 结果展示
         print(text)
         pie_chart.render_to_file('output\\temp.svg')
@@ -182,6 +186,7 @@ class 个股主营业务分析_按地区(个股分析模板):
             if len(df) == 0:
                 date = self._get_last_season_end(date)
                 df = pro.fina_mainbz(ts_code=self.code, period=self.tushare_date_format(date), type='D')
+        print(df)
         text = self.code_name + '（' + self.code + '）的主营业务构成为（数据截止为'+date+'）：\n'
         df = df.sort_values(by='bz_sales', axis=0, ascending=False)
         # 预处理，将None转化为np.nan
@@ -191,26 +196,92 @@ class 个股主营业务分析_按地区(个股分析模板):
         df['bz_sales_ratio'] = df['bz_sales'] / np.sum(df['bz_sales'].values)  # 计算营业收入占比
         df['bz_profit_ratio'] = df['bz_profit'] / df['bz_sales']  # 计算毛利率
         # 文字生成部分
+        table = prettytable.PrettyTable(['业务名称', '销售收入', '销售成本', '销售毛利', '销售毛利率', '销售收入占比'])
         for i in range(len(df)):
             data = df.iloc[i].values
-            text_temp = '业务名称：%s，\t销售收入：%.2f万元，\t销售成本：%.2f万元，\t销售毛利：%.2f万元，\t销售毛利率：%.2f%%，\t销售收入占比：%.2f%%；\n'
-            text_temp = text_temp % (data[2], data[3]/10000.0, data[5]/10000.0, data[4]/10000.0, data[8]*100.0, data[7]*100.0)
-            text = text + text_temp
+            table.add_row(['%s' % data[2], '%.2f万元' % (data[3] / 10000.0), '%.2f万元' % (data[5] / 10000.0), '%.2f万元' % (data[4] / 10000.0), '%.2f%%' % (data[8] * 100.0), '%.2f%%' % (data[7] * 100.0)])
+        text = text + str(table) + '\n'
         # 画图部分
         pie_chart = pygal.Pie()
         pie_chart.title = self.code_name + '（' + self.code + '）的主营业务百分比构成图（数据截止为'+date+'）'
         for i in range(len(df)):
             data = df.iloc[i].values
             pie_chart.add(data[2], data[7]*100.0)
+        # 总结内容
+        if len(df) > 0:
+            summary_text = self._get_summary(df, self.code_name)
+            text = text + summary_text
+        else:  # 无数据披露的情形
+            text = text + '无数据！\n'
         # 结果展示
         print(text)
         pie_chart.render_to_file('output\\temp.svg')
         result = {'text': text, 'image': [pie_chart]}
         return result
 
+    @staticmethod
+    def _get_summary(df, code_name):
+        bz_item = df['bz_item'].values
+        bz_sales_ratio = df['bz_sales_ratio'].values
+        bz_sales_ratio_cumsum = np.cumsum(bz_sales_ratio)
+        first_bigger_index = int(np.where(bz_sales_ratio_cumsum > 0.90)[0][0])  # 第一个求和大于90%的指标，包括指标本身
+        text = code_name + '的主要业务区域有：'
+        for i in range(first_bigger_index + 1):
+            text = text + bz_item[i]
+            if i == first_bigger_index:
+                text = text + '；\n'
+            else:
+                text = text + '，'
+        for i in range(0, first_bigger_index + 1):  # 提取核心业务区域，主要业务区域的50%权重为核心业务区域
+            core_index = first_bigger_index - i
+            if bz_sales_ratio[core_index] / bz_sales_ratio[0] >= 0.5:
+                break
+        text = text + '其中'
+        for i in range(0, core_index + 1):
+            text = text + bz_item[i]
+            if i == core_index:
+                text = text + '是核心业务区域；'
+            else:
+                text = text + '、'
+        for i in range(core_index + 1, first_bigger_index + 1):
+            text = text + bz_item[i]
+            if i == first_bigger_index:
+                text = text + '是辅助业务区域；'
+            else:
+                text = text + '、'
+        if text[-1] == '；':
+            text = text[:-1] + '。'
+        text = text + '\n'
+        if (core_index + 1) > 3:
+            text = text + code_name + '的业务区域非常多元化，业务在地域分布上比较分散'
+            if core_index == first_bigger_index:
+                text = text + '。'
+            else:
+                text = text + '，公司还少量辅助业务区域。'
+        elif (core_index + 1) == 3:
+            text = text + code_name + '的业务区域比较多元化，属于三核心业务区域类型'
+            if core_index == first_bigger_index:
+                text = text + '。'
+            else:
+                text = text + '，公司还少量辅助业务区域。'
+        elif (core_index + 1) == 2:
+            text = text + code_name + '的业务集中在两个区域，属于双核心业务区域类型'
+            if core_index == first_bigger_index:
+                text = text + '。'
+            else:
+                text = text + '，公司还少量辅助业务区域。'
+        else:
+            text = text + code_name + '的业务集中在一个区域，属于单核心业务区域类型'
+            if core_index == first_bigger_index:
+                text = text + '。'
+            else:
+                text = text + '，公司还少量辅助业务区域。'
+        text = text + '\n'
+        return text
+
 
 if __name__ == '__main__':
-    code = '002565.SZ'
+    code = '000063.SZ'
     date = '2019-01-01'
     model = 个股主营业务分析_按产品(code, date)
     model.output()
