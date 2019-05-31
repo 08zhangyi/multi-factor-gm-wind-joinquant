b51a156987228bef795befa3429efcdb69ab734e
08zhang <395871987@qq.com>
2019-05-31 15:15:36 +0800

    日常维护

--- 市场分析工具/市场舆情监控/sina_version.py
+++ 市场分析工具/市场舆情监控/sina_version.py
@@ -4,8 +4,8 @@ import time
 import pyecharts
 import math
 
-start_date = '2019-05-16 15:00:00'
-end_date = '2019-05-17 08:30:00'
+start_date = '2019-05-30 15:00:00'
+end_date = '2019-05-31 08:30:00'
 # 百度配置
 APP_ID = '10709883'
 API_KEY = 'DP7yZde5EK2MEKLzcjzwCCp5'
