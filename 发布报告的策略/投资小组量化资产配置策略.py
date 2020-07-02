90e23866c6047f732bdd57fbcea7f99f839f7caa
08zhang <395871987@qq.com>
2020-07-02 14:25:50 +0800

    日常维护

--- 发布报告的策略/投资小组量化资产配置策略.py
+++ 发布报告的策略/投资小组量化资产配置策略.py
@@ -7,9 +7,26 @@ from utils import list_wind2jq
 from 持仓配置 import 风险预算组合_模块求解基本版_带约束
 
 # 统一设定调仓日，选股为前一天收盘后
-DATE = '2020-06-12'
+DATE = '2020-06-24'
 # 债券品种的比例调整
-BOND_CORRECTIONG = {'160618.XSHE': 0.07, '161713.XSHE': 0.02, '161716.XSHE': 0.09, '167501.XSHE': 0.0, '511260.XSHG': 0.05, '511010.XSHG': -0.23}
+BOND_ADJUST = {'160618.XSHE': 0.06, '161713.XSHE': 0.02, '161716.XSHE': 0.09, '511260.XSHG': 0.06, '511010.XSHG': -0.23}
+# A股手动调整比例
+STOCK_ADJUST = 0.0
+
+
+# 调整函数
+def adjust_weights(stock_weights, bond_adjust, stock_adjust):
+    # A股部分的调整，同步等比例调整债券、外盘、黄金的比例
+    stock_weights['510300.XSHG'] = stock_weights.get('510300.XSHG', 0.0) + stock_adjust
+    adjusted_codes = ['511010.XSHG', '513050.XSHG', '513100.XSHG', '513500.XSHG', '518880.XSHG']
+    adjusted_full_ratio = np.sum([stock_weights[code] for code in adjusted_codes])
+    for code in adjusted_codes:
+        stock_weights[code] = stock_weights[code] * ((adjusted_full_ratio - stock_adjust) / adjusted_full_ratio)
+    # 债券部分的调整，加入信用债和长期国债，并调整比例
+    for bond_code in bond_adjust:
+        stock_weights[bond_code] = stock_weights.get(bond_code, 0.0) + bond_adjust[bond_code]
+    return stock_weights
+
 
 # 自动读取风险预算数据
 FILE_PATH = 'C:\\Users\\pc\\Desktop\\策略计算表temp.xlsx'  # 参考data\策略计算表example.xlsx
@@ -26,7 +43,7 @@ for S in S_all:
         value_code = table.cell(i, S[0]).value
         value_ratio = table.cell(i, S[1]).value
         if value_code != '':
-            if value_code in ['160618.SZ', '161713.SZ', '161716.SZ', '167501.SZ', '511260.SH']:  # 债券类不参与权重计算
+            if value_code in ['160618.SZ', '161713.SZ', '161716.SZ', '511260.SH']:  # 债券类不参与权重计算
                 pass
             elif value_code in ['511010.SH']:  # 债券类参与权重计算
                 stock_pool.append(value_code)
@@ -48,12 +65,7 @@ risk_budget_0 = S_result[0][1]
 risk_bounds_0 = S_result[0][2]
 stock_weights_0 = 风险预算组合_模块求解基本版_带约束(stock_pool_0, DATE, risk_budget=risk_budget_0, bounds=risk_bounds_0).get_weights()
 # 加入信用债品种，对国债配置比例进行修正
-stock_weights_0['160618.XSHE'] = BOND_CORRECTIONG['160618.XSHE']
-stock_weights_0['161713.XSHE'] = BOND_CORRECTIONG['161713.XSHE']
-stock_weights_0['161716.XSHE'] = BOND_CORRECTIONG['161716.XSHE']
-stock_weights_0['167501.XSHE'] = BOND_CORRECTIONG['167501.XSHE']
-stock_weights_0['511260.XSHG'] = BOND_CORRECTIONG['511260.XSHG']
-stock_weights_0['511010.XSHG'] = BOND_CORRECTIONG['511010.XSHG'] + stock_weights_0['511010.XSHG']
+stock_weights_0 = adjust_weights(stock_weights_0, BOND_ADJUST, STOCK_ADJUST)
 # 输出文字信息
 string_output_0 = '策略0的风险预算权重为：\n'
 string_output_0 += str(dict(zip(stock_pool_0, risk_budget_0))) + '\n'
@@ -67,12 +79,7 @@ risk_budget_1 = S_result[1][1]
 risk_bounds_1 = S_result[1][2]
 stock_weights_1 = 风险预算组合_模块求解基本版_带约束(stock_pool_1, DATE, risk_budget=risk_budget_1, bounds=risk_bounds_1).get_weights()
 # 加入信用债品种，对国债配置比例进行修正
-stock_weights_1['160618.XSHE'] = BOND_CORRECTIONG['160618.XSHE']
-stock_weights_1['161713.XSHE'] = BOND_CORRECTIONG['161713.XSHE']
-stock_weights_1['161716.XSHE'] = BOND_CORRECTIONG['161716.XSHE']
-stock_weights_1['167501.XSHE'] = BOND_CORRECTIONG['167501.XSHE']
-stock_weights_1['511260.XSHG'] = BOND_CORRECTIONG['511260.XSHG']
-stock_weights_1['511010.XSHG'] = BOND_CORRECTIONG['511010.XSHG'] + stock_weights_1['511010.XSHG']
+stock_weights_1 = adjust_weights(stock_weights_1, BOND_ADJUST, STOCK_ADJUST)
 # 输出文字信息
 string_output_1 = '策略1的风险预算权重为：\n'
 string_output_1 += str(dict(zip(stock_pool_1, risk_budget_1))) + '\n'
@@ -86,12 +93,7 @@ risk_budget_2 = S_result[2][1]
 risk_bounds_2 = S_result[2][2]
 stock_weights_2 = 风险预算组合_模块求解基本版_带约束(stock_pool_2, DATE, risk_budget=risk_budget_2, bounds=risk_bounds_2).get_weights()
 # 加入信用债品种，对国债配置比例进行修正
-stock_weights_2['160618.XSHE'] = BOND_CORRECTIONG['160618.XSHE']
-stock_weights_2['161713.XSHE'] = BOND_CORRECTIONG['161713.XSHE']
-stock_weights_2['161716.XSHE'] = BOND_CORRECTIONG['161716.XSHE']
-stock_weights_2['167501.XSHE'] = BOND_CORRECTIONG['167501.XSHE']
-stock_weights_2['511260.XSHG'] = BOND_CORRECTIONG['511260.XSHG']
-stock_weights_2['511010.XSHG'] = BOND_CORRECTIONG['511010.XSHG'] + stock_weights_2['511010.XSHG']
+stock_weights_2 = adjust_weights(stock_weights_2, BOND_ADJUST, STOCK_ADJUST)
 # 输出文字信息
 string_output_2 = '策略2的风险预算权重为：\n'
 string_output_2 += str(dict(zip(stock_pool_2, risk_budget_2))) + '\n'
